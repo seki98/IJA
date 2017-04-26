37e4e5134664ab3811a407054bb93657cd163724
sekiDell <seki98@gmail.com>
2017-04-26 21:10:39 +0200

    gui activated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IJA
+run 'ant run' in the directory where build.xml is located. Gui from src/ija/GUISwing.java is loaded.
 VUT FIT IJA
--- src/ija/GUISwing.java
+++ src/ija/GUISwing.java
@@ -12,28 +12,28 @@ public class GUISwing
   private static void createGUI()
   {
     PutCommand cmd = new PutCommand();
-//    JFrame frame = new JFrame("Solitaire Klondike");
-//    frame.setPreferredSize(new Dimension(500, 500));
-//    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//    JPanel panel = new JPanel();
-//    panel.setBackground(Color.green);
-//    panel.setLayout(null);
-//
-//    JLabel lab1 = new JLabel("card 1");
-//    lab1.setBounds(50, 50, 100, 100); 
-//    lab1.addMouseListener(new MouseAdapter()
-//    {
-//        public void mousePressed(MouseEvent arg0)
-//        {
-//          lab1.setBounds(100,100,100,100);
-//        }
-//    });
-//    panel.add(lab1);
-//    frame.getContentPane().add(panel);
-//
-//    frame.pack();
-//    frame.setVisible(true);
+    JFrame frame = new JFrame("Solitaire Klondike");
+    frame.setPreferredSize(new Dimension(500, 500));
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel panel = new JPanel();
+    panel.setBackground(Color.green);
+    panel.setLayout(null);
+
+    JLabel lab1 = new JLabel("card 1");
+    lab1.setBounds(50, 50, 100, 100); 
+    lab1.addMouseListener(new MouseAdapter()
+    {
+        public void mousePressed(MouseEvent arg0)
+        {
+          lab1.setBounds(100,100,100,100);
+        }
+    });
+    panel.add(lab1);
+    frame.getContentPane().add(panel);
+
+    frame.pack();
+    frame.setVisible(true);
   }
 
   public static void main(String [] args)
