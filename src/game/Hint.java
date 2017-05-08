d99977314104dd3ba2edab614c472e4d501d398e
Sebastian Kisela <skisela@redhat.com>
2017-05-08 23:01:57 +0200

    doc fix

--- src/game/Hint.java
+++ src/game/Hint.java
@@ -2,19 +2,20 @@ package src.game;
 import src.ija2016.model.cards.CardDeck;
 import src.ija2016.model.cards.Card;
 
-/**
- * This class represents Hints which are given to the player.
- * @param src - the index of the source stack from the beginning
- * @param tar - the index of the target stack from the beginning
- * @param cmd - command to be done
- * @param card - card that is to be mvoed
- */
 public class Hint
 {
     int src;
     int tar;
     Card c;
     int cmd;
+
+    /**
+     * This class represents Hints which are given to the player.
+     * @param src - the index of the source stack from the beginning
+     * @param tar - the index of the target stack from the beginning
+     * @param c -card that is to be mvoed
+     * @param cmd - command to be done
+     */
     public Hint(int src, int tar, Card c, int cmd)
     {
         this.src = src;
